8455397308536b5f94d75ec823823077c9b490aa
avishka964 <avishkakapuruge@gmail.com>
2020-12-30 08:08:55 +0530

    props and child props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react"
 import "./App.css"
+import Person from "./Person/Person"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <Person name="Brad" age="28" />
+        <Person name="Max" age="29">
+          My Hobbies: Racing
+        </Person>
+        <Person name="Sam" age="26" />
       </div>
     )
   }
