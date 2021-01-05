091b679c020e05586ba73541dd7723797c818450
avishka964 <avishkakapuruge@gmail.com>
2021-01-05 08:15:05 +0530

    class and functional lifecycle

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,17 +1,41 @@
-import React from "react"
+import React, { Component } from "react"
 import Person from "./Person/Person"
 
-const Persons = (props) =>
-  props.persons.map((persons, index) => {
-    return (
-      <Person
-        click={() => props.clicked(index)}
-        name={persons.name}
-        age={persons.age}
-        key={persons.id}
-        change={(event) => props.changed(event, persons.id)}
-      />
-    )
-  })
+class Persons extends Component {
+  // static getDerivedStateFromProps(props, state) {
+  //   console.log("Person.js getDerivedStateFromProps")
+  //   return state
+  // }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("Persons.js shouldComponentUpdate")
+    return true
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log("Persons.js getSnapshotBeforeUpdate")
+    return { message: "Snapshot!" }
+  }
+
+  componentDidUpdate(preProps, preState, snapshot) {
+    console.log("Persons.js componentDidUpdate")
+    console.log(snapshot)
+  }
+
+  render() {
+    console.log("Persons.js rendering...")
+    return this.props.persons.map((persons, index) => {
+      return (
+        <Person
+          click={() => this.props.clicked(index)}
+          name={persons.name}
+          age={persons.age}
+          key={persons.id}
+          change={(event) => this.props.changed(event, persons.id)}
+        />
+      )
+    })
+  }
+}
 
 export default Persons
--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,11 @@ import Persons from "../components/Persons/Persons"
 import Cockpit from "../components/Cockpit/Cockpit"
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    console.log("App.js constructor")
+  }
+
   state = {
     persons: [
       { id: "dfd1", name: "Brad", age: 30 },
@@ -12,7 +17,25 @@ class App extends Component {
     ],
     otherState: "some other value",
     showPersons: false,
-    alt: true,
+    showCockpit: true,
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log("App.js getDerivedStateFromProps", props)
+    return state
+  }
+
+  componentDidMount() {
+    console.log("App.js componentDidMount")
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("App.js shouldComponentUpdate")
+    return true
+  }
+
+  componentDidUpdate() {
+    console.log("App.js componentDidUpdate")
   }
 
   nameChangeHandler = (event, id) => {
@@ -45,6 +68,7 @@ class App extends Component {
   }
 
   render() {
+    console.log("App.js render...")
     let persons = null
 
     if (this.state.showPersons) {
@@ -59,12 +83,21 @@ class App extends Component {
 
     return (
       <div className={classes.App}>
-        <Cockpit
-          title={this.props.appTitle}
-          showPersons={this.state.showPersons}
-          persons={this.state.persons}
-          clicked={this.togglePersonsHandler}
-        />
+        <button
+          onClick={() => {
+            this.setState({ showCockpit: false })
+          }}>
+          Remove Cockpit
+        </button>
+
+        {this.state.showCockpit ? (
+          <Cockpit
+            title={this.props.appTitle}
+            showPersons={this.state.showPersons}
+            persons={this.state.persons}
+            clicked={this.togglePersonsHandler}
+          />
+        ) : null}
         {persons}
       </div>
     )
